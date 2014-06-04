f7f43bfda8e7c3a08a7fc154b0f265dceb224cda
jjlangholtz@gmail.com <jjlangholtz@gmail.com>
2014-06-04 09:53:59 -0400

    Update style and refactor

--- guessing_game.rb
+++ guessing_game.rb
@@ -1,28 +1,31 @@
-class Game
+# Guessing game with five guesses to pick a random number 1 - 100
+class GuessingGame
   def initialize
     @number = rand(100) + 1
     @won = false
   end
 
-  def start
-    puts 'You have five chances to guess my number!'
-    print "Guess a number between 1 and 100 > "
-  end
-
   def play
     start
-    5.times do
-      guess = gets.chomp.to_i
-      if guess == @number
-        @won = true
-        break
-      elsif guess > @number
-        puts 'Too high'
-      else
-        puts 'Too low'
-      end
-    end
+    5.times { @won == false ? guess_number : break }
     finish
+  end
+
+  def start
+    puts 'You have five chances to guess my number!'
+    print 'Guess a number between 1 and 100 > '
+  end
+
+  def guess_number
+    guess = gets.to_i
+    case
+    when guess == @number
+      @won = true
+    when guess > @number
+      puts 'Too high'
+    when guess < @number
+      puts 'Too low'
+    end
   end
 
   def finish
@@ -34,5 +37,5 @@ class Game
   end
 end
 
-game = Game.new
+game = GuessingGame.new
 game.play
